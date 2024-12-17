2ae542e49bcf1002ad3b87a7d6fe90f5edb484e1
FelipeSouza95 <felipemoraes.recreio@gmail.com>
2024-12-17 16:56:47 -0300

    Criação da estrutura dos pacotes.

---
+++ src/main/java/com/nunesbarweb/NunesBarWebApplication.java
@@ -0,0 +1,13 @@
+package com.nunesbarweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NunesBarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NunesBarWebApplication.class, args);
+	}
+
+}
