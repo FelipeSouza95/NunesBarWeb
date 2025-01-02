721fcebcfd1c9fbdc697050b6712766940b80bc0
FelipeSouza95 <felipemoraes.recreio@gmail.com>
2025-01-02 12:58:45 -0300

    Alteração do Pom

--- src/main/java/com/nunesbarweb/NunesBarWebApplication.java
+++ src/main/java/com/nunesbarweb/NunesBarWebApplication.java
@@ -2,12 +2,20 @@ package com.nunesbarweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class NunesBarWebApplication {
+public class NunesBarWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NunesBarWebApplication.class, args);
-	}
+    // Método principal para rodar a aplicação
+    public static void main(String[] args) {
+        SpringApplication.run(NunesBarWebApplication.class, args);
+    }
 
+    // Sobrescreve o método configure para suportar o empacotamento como WAR
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NunesBarWebApplication.class);
+    }
 }
